79a295e90923748312e2e8dbd982c9f9d5eaec15
Mark Hernandez <m.hern1415@gmail.com>
2019-06-10 20:00:00 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,9 +4,10 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
 
-binding.pry
+
 
 create_project_hash
 
